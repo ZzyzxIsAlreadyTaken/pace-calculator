6e35a6f1c1cb9e85a7553f916f09f8150c3a8766
ZzyzxIsAlreadyTaken <theoz_@hotmail.com>
2025-06-19 14:19:35 +0200

    SVG Icon for header import to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import UnitToggle from "./UnitToggle";
+import runningIcon from "../assets/running.svg";
 
 interface HeaderProps {
   unit: "metric" | "imperial";
@@ -10,7 +11,7 @@ export default function Header({ unit, onUnitToggle }: HeaderProps) {
     <header className="flex items-center justify-between px-4 py-2.5 sm:px-8 sm:py-4 bg-white border-b border-gray-200">
       <div className="flex items-center space-x-2 sm:space-x-3">
         <img
-          src="/src/assets/running.svg"
+          src={runningIcon}
           alt="Pace Calculator Icon"
           className="w-7 h-7 sm:w-8 sm:h-8"
         />
